1d648c0409cf9d9fe70d6b61bc9f8212851f0e52
Luigi Masturzo <luigimasturzo@host-131-114-212-241.m.unipi.it>
2019-10-21 17:20:31 +0200

    ciao

---
+++ splrand/pdf.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Luca Baldini (luca.baldini@pi.infn.it)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Core logic for the pdf definition.
+"""
+
+#VEDI COSA SONO LE VARIABILI DAMBIENTE!!!!!
+import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+
+
+
+class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
+
+    """Class describing a probability density function.
+    """
+
+    def __init__(self, x, y):
+        """Constructor.
+        """
+        InterpolatedUnivariateSpline.__init__(self, x, y)
+        ycdf = np.array([self.integral(x[0], xcdf) for xcdf in x])
+        self.cdf = InterpolatedUnivariateSpline(x, ycdf)
+        # Need to make sure that the vector I am passing to the ppf spline as
+        # the x values has no duplicates---and need to filter the y
+        # accordingly.
+        xppf, ippf = np.unique(ycdf, return_index=True)
+        yppf = x[ippf]
+        self.ppf = InterpolatedUnivariateSpline(xppf, yppf)
+
+    def prob(self, x1, x2):
+        """Return the probability for the random variable to be included
+        between x1 and x2.
+        """
+        return self.cdf(x2) - self.cdf(x1)
+
+    def rnd(self, size=1000):
+        """Return an array of random values from the pdf.
+        """
+        return self.ppf(np.random.uniform(size=size))
